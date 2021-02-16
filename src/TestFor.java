9425e2fa4af3829df5f80658780bb5053fdcb552
SergeyBordulev <bordulev@yahoo.com>
2021-02-16 12:21:38 +0700

    Initial commit

---
+++ src/TestFor.java
@@ -0,0 +1,19 @@
+public class TestFor {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+
+        for (int i = 7; i <= 121; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+
+        for (int i = 100; i >= 30; i--) {
+            System.out.println(i);
+        }
+    }
+}
